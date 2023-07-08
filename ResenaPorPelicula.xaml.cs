cd20b7f70fd099c774470df6816d648613e6df19
Ariel Sebastian Raura Diaz <ariel.raura@udla.edu.ec>
2023-07-08 01:33:18 -0500

    Implementacion API YOutube

--- ResenaPorPelicula.xaml.cs
+++ ResenaPorPelicula.xaml.cs
@@ -15,6 +15,7 @@ namespace AR_JC_ProyectoP2
         private ResenaPorPeliculaViewModel _viewModel;
         public listaUsuarios user;
 
+
         public ResenaPorPelicula(listaPelicula pelicula, listaUsuarios usuario)
         {
             InitializeComponent();
@@ -56,6 +57,75 @@ namespace AR_JC_ProyectoP2
             var button = (Button)sender;
             var resena = button.BindingContext as listaResena;
             await Navigation.PushAsync(new ComentarioPorResena(resena, user));
+        }
+
+        private async void ButtonVerTrailer_Clicked(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            listaPelicula pelicula = _viewModel.Pelicula;
+            if (pelicula != null)
+            {
+                // Obtener el videoId utilizando la API de YouTube
+                string videoId = await GetYouTubeVideoId(pelicula.nombre);
+
+                if (!string.IsNullOrEmpty(videoId))
+                {
+                    // Construir la URL del video
+                    string videoUrl = $"https://www.youtube.com/watch?v={videoId}";
+
+
+                    // Genera el código HTML para incrustar el video de YouTube
+                    string html = $"<html><body><iframe width='100%' height='100%' src='https://www.youtube.com/embed/{videoId}' frameborder='0' allow='autoplay; encrypted-media' allowfullscreen></iframe></body></html>";
+
+                    // Carga el código HTML en el WebView
+                    webView.Source = new HtmlWebViewSource { Html = html };
+                    //trailerLayout.IsVisible = true;
+                }
+                else
+                {
+                    await DisplayAlert("Error", "No se encontró el video de YouTube para la película.", "OK");
+                }
+            }
+            // Obtén el ID del video de YouTube para la película seleccionada
+
+        }
+
+
+
+
+        private async Task<string> GetYouTubeVideoId(string movieName)
+        {
+
+            try
+            {
+
+                string apiKey = "AIzaSyD4GIJFGK5gYx7emy3KDXhscqtuv4EkmCg";
+
+                using (HttpClient client = new HttpClient())
+                {
+                    // Realizar la solicitud a la API de búsqueda de YouTube
+                    string requestUrl = $"https://www.googleapis.com/youtube/v3/search?part=snippet&q={Uri.EscapeDataString(movieName)}%20trailer&type=video&key={apiKey}";
+                    HttpResponseMessage response = await client.GetAsync(requestUrl);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string responseJson = await response.Content.ReadAsStringAsync();
+                        dynamic responseObject = JsonConvert.DeserializeObject(responseJson);
+
+                        if (responseObject != null && responseObject.items != null && responseObject.items.Count > 0)
+                        {
+                            string videoId = responseObject.items[0].id.videoId;
+                            return videoId;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al obtener el ID del video de YouTube: {ex.Message}");
+            }
+
+            return null;
         }
     }
 }
